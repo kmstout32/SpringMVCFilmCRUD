d39bcb1dcc2cc6085ce1b5fbe4c4576783484211
Kira Stout <kmstout08@gmail.com>
2023-03-03 19:11:51 -0500

    Created FilmDoaJdbcImpl and actor class

---
+++ src/main/java/com/skilldistillery/film/data/FilmDaoJdbcImpl.java
@@ -0,0 +1,34 @@
+package com.skilldistillery.film.data;
+
+import java.util.List;
+
+import com.skilldistillery.film.entities.Actor;
+import com.skilldistillery.film.entities.Film;
+
+public class FilmDaoJdbcImpl implements FilmDAO {
+
+	@Override
+	public Film findFilmById(int filmId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Actor findActorById(int actorId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Actor> findActorsByFilmId(int filmId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Film> findFilmByActorId(int filmId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
