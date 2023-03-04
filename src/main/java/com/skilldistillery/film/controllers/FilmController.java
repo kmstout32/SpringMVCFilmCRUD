205a4d8beb584f8f9a00586611e1c085e8fb5165
Kira Stout <kmstout08@gmail.com>
2023-03-04 12:56:50 -0500

    updated test method

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -33,17 +33,12 @@ public class FilmController {
 
 	}
 
-//	@RequestMapping(path = { "film.do" })
-//	public String createFilm(Model model, Film film) {
-//		Film filmCreate = new Film (0, "fdsa", "fdsa", 1996, 5, 6, 2.6, 5, 9.9, "G", "Trailers");
-//		Film filmCreated = filmDao.createFilm(filmCreate);
-//		model.addAttribute(filmCreated);
-//		return "filmID";
-//	}
 	@RequestMapping(path = { "film.do" })
 	public String createFilm(Model model, Film film) {
-		Film filmCreated = filmDao.createFilm();
+		Film filmCreate = new Film (0, "fdsa", "fdsa", 1996, 5, 6, 2.6, 5, 9.9, "G", "Trailers");
+		Film filmCreated = filmDao.createFilm(filmCreate);
 		model.addAttribute(filmCreated);
 		return "filmID";
 	}
+
 }
