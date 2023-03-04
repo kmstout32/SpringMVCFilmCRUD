73e6fd271e47013f06fedee7157782dfc8a095fb
Edwin <Flattoedwin@gmail.com>
2023-03-03 20:47:42 -0500

    update

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -12,12 +12,23 @@ import com.skilldistillery.film.entities.Film;
 
 @Controller
 public class FilmController {
-	@Autowired
-	private FilmDAO filmDao;
+@Autowired
+ private FilmDAO filmDao;
 
-	@RequestMapping(path = { "/", "home.do" })
-	public String goHome(Model model) {
-		return "home";
+@RequestMapping(path = {"/","home.do"})
+public String goHome(Model model) throws SQLException {
+	Film TEST =  filmDao.findFilmById(1);
+	model.addAttribute("TESTFILM",TEST);
+	return "home";
+}
+
+static {
+	try {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+	} catch (ClassNotFoundException e) {
+		e.printStackTrace();
+		System.err.println("Error loading MySQL Driver");
+		throw new RuntimeException("Unable to load MySQL Driver class");
 	}
-
+}
 }
