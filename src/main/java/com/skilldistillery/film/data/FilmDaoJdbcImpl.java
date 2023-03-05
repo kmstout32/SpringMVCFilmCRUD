bba2735c75565cfcd56113af0c344422db184599
Edwin <Flattoedwin@gmail.com>
2023-03-05 12:08:11 -0500

    updated the updatefilm dao

--- src/main/java/com/skilldistillery/film/data/FilmDaoJdbcImpl.java
+++ src/main/java/com/skilldistillery/film/data/FilmDaoJdbcImpl.java
@@ -496,9 +496,44 @@ public class FilmDaoJdbcImpl implements FilmDAO {
 
 	@Override
 	public Film updateFilm(int filmId, Film film) {
-		// TODO Auto-generated method stub
-		return null;
+	    Connection conn = null;
+	    try {
+	        conn = DriverManager.getConnection(URL, user, pass);
+	        conn.setAutoCommit(false); // START TRANSACTION
+	        String sql = "UPDATE film SET title =?, description=?, release_year=?, language_id=?, rental_duration=?, rental_rate=? ,length=?, replacement_cost=?, rating=? ,special_features=?" + " WHERE id=?";
+	        PreparedStatement stmt = conn.prepareStatement(sql);
+	        stmt.setString(1, film.getTitle());
+	        stmt.setString(2, film.getDescription());
+	        stmt.setInt(3, film.getReleaseYear());
+	        stmt.setInt(4, film.getLanguageId());
+	        stmt.setInt(5, film.getRentalDuration());
+	        stmt.setDouble(6, film.getRentalRate());
+	        stmt.setInt(7, film.getLength());
+	        stmt.setDouble(8, film.getReplacementCost());
+	        stmt.setString(9, film.getRating());
+	        stmt.setString(10, film.getSpecialFeature());
+
+	        int updateCount = stmt.executeUpdate();
+	        if (updateCount == 1) {
+	           
+	           
+	            conn.commit(); // COMMIT TRANSACTION
+	        }
+	    } catch (SQLException sqle) {
+	        sqle.printStackTrace();
+	        if (conn != null) {
+	            try {
+	                conn.rollback();
+	            } // ROLLBACK TRANSACTION ON ERROR
+	            catch (SQLException sqle2) {
+	                System.err.println("Error trying to rollback");
+	            }
+	        }
+	        return null;
+	    }
+	    return film;
 	}
+
 
 	static {
 		try {
